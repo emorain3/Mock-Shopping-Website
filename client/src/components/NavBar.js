a582073a5652a9e46e7a1521e1465f89f4fbedb6
ecclesia-morain <emorain@live.com>
2019-01-29 23:13:16 -0500

    HomePage v1 DONE. MOBILE-READY TOOOOOO

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,17 +4,17 @@ import styled from 'styled-components';
 import logo from '../images/brand_icon_transparent.png'
 
 let navStyle = {
-    position: 'relative', 
-    zIndex: '980',
-  };
+    paddingBottom: '1vw',
+};
 
 let navBkgndClr = {
     width: '100vw', 
+    zIndex: '500',
 };
 
 let Logo = styled.img`
     margin-top: 1vw;
-    margin-right: 5vw;
+    margin-right: 4vw;
     margin-left: 2vw;
     height: 86px;
 `
@@ -39,15 +39,22 @@ class NavBar extends Component {
                             <Link to={"/america"}> <Logo src={logo} alt="Brand Logo" /> </Link>
 
                             <ul class="uk-navbar-nav">
+                                
                                 <li>
                                     <a href="#"> Clothing </a>
-                                    <div class="uk-navbar-dropdown">
-                                        <ul class="uk-nav uk-navbar-dropdown-nav">
-                                            <li class="uk-nav-header"> Tops </li>
-                                            <li><a href="#"> </a> T-shirts </li>
-                                            <li><a href="#"> </a> Blouses </li>
+                                    <div class="uk-navbar-dropdown uk-navbar-dropdown-width-2">
+                                        <div class="uk-navbar-dropdown-grid uk-child-width-1-2" uk-grid>
+                                            <div>
+                                                <ul class="uk-nav uk-navbar-dropdown-nav">
 
-                                        </ul>
+                                                    <li class="uk-nav-header"> Tops </li>
+                                                    <li> <a href="/america/clothing/tshirts"> T-shirts </a></li>
+                                            
+            
+                                                    <li> <a href="/america/clothing/blouses"> Blouses </a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
                                 </li>
                                 <li>
@@ -58,10 +65,10 @@ class NavBar extends Component {
                                                 <ul class="uk-nav uk-navbar-dropdown-nav">
 
                                                     <li class="uk-nav-header">Office Essentials</li>
-                                                    <li><a href="#"> Notebooks </a></li>
+                                                    <li><a href="/america/office"> Notebooks and Stationery </a></li>
                                             
                                                     <li class="uk-nav-divider"></li>
-                                                    <li class="uk-active"><a href="#"> Purses </a></li>
+                                                    <li class="uk-nav-header"><a href="/america/purses"> Purses </a></li>
                                                 </ul>
                                             </div>
                                         </div>
