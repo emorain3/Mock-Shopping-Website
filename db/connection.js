b554e9c21745abf9dc11a51335ff252bc7470d31
emorain3 <8389505+emorain3@users.noreply.github.com>
2023-07-25 17:32:07 -0400

    Got it to run. Need to host on Heroku. There are many bugs. Will need slight recomp.

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI) 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, 
+                                            useUnifiedTopology: true, 
+                                            useCreateIndex: true,
+                                            useFindAndModify: false }) 
 
 mongoose.connection.once('open', () => { 
     console.log(`Mongoose has connected to MongoDB`) 
